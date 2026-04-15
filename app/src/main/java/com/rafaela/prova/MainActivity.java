c3f9b9d90ce7116ecc09bd5b2d17560d7685bf52
RafaelaMichelin <RafaelaMichelin@gmail.com>
2026-04-15 20:35:24 -0300

    Adicionando cofiguração do botão report na main activity

--- app/src/main/java/com/rafaela/prova/MainActivity.java
+++ app/src/main/java/com/rafaela/prova/MainActivity.java
@@ -1,5 +1,6 @@
 package com.rafaela.prova;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
@@ -79,6 +80,11 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(MainActivity.this, "Preencha os campos obrigatórios!", Toast.LENGTH_SHORT).show();
         }
     });
+
+        // Configurar o botão de relatório para abrir a tela de relatório
+        buttonReport.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, ReportActivity.class))
+        );
 
     }
 }
