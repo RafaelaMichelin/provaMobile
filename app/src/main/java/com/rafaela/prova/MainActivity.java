63b63d01b1aaafb4912524b6a0cf6f2dfb427007
RafaelaMichelin <RafaelaMichelin@gmail.com>
2026-04-15 19:40:58 -0300

    Criando método OnCreate, iniciando campos de texto e botões

--- app/src/main/java/com/rafaela/prova/MainActivity.java
+++ app/src/main/java/com/rafaela/prova/MainActivity.java
@@ -1,6 +1,7 @@
 package com.rafaela.prova;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
@@ -16,4 +17,22 @@ public class MainActivity extends AppCompatActivity {
 
     //Objeto para interagir com o banco de dados(DAO)
     private ProductDao productDao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        //Iniciar os campos de entrada de dados do Layout
+        editTextName = findViewById(R.id.editTextName);
+        editTextCode = findViewById(R.id.editTextCode);
+        editTextprice = findViewById(R.id.editTextPrice);
+        editTextAmount = findViewById(R.id.editTextAmount);
+
+        //Iniciar botões do Layout
+        Button buttonSave = findViewById(R.id.buttonSave);
+        Button buttonReport = findViewById(R.id.buttonReport);
+
+    }
 }
