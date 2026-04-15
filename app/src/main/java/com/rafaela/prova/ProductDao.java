d457c5b47bfe08c04361132220c570e9c6455362
RafaelaMichelin <RafaelaMichelin@gmail.com>
2026-04-15 20:51:52 -0300

    Implementando a busca de produtos no banco de dados e salvando em uma lista para exibir os campos buscados na tela de relatorio

--- app/src/main/java/com/rafaela/prova/ProductDao.java
+++ app/src/main/java/com/rafaela/prova/ProductDao.java
@@ -2,11 +2,19 @@ package com.rafaela.prova;
 
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
 
 @Dao
 public interface ProductDao {
-    //Método inserir dados
-
+    //Metodo inserir dados
     @Insert
     void Insert(Product product);
+
+    //Metodo para buscar todos os produtos no BD
+    @Query("SELECT * FROM product")
+    //Salvar o que foi selecionado em uma lista
+    List<Product> getAllProducts();
+
 }
--- app/src/main/java/com/rafaela/prova/ReportActivity.java
+++ app/src/main/java/com/rafaela/prova/ReportActivity.java
@@ -1,4 +1,54 @@
 package com.rafaela.prova;
 
-public class ReportActivity {
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
+import java.util.List;
+
+public class ReportActivity extends AppCompatActivity {
+    //Campo de texto onde os dados do banco serão exibidos
+    private TextView textViewReport;
+
+    //Metodo create
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Define o layout XML da tela relatório
+        setContentView(R.layout.activity_report);
+        textViewReport = findViewById(R.id.textViewReport);
+
+        //Encontrar o Botão de voltar
+        Button btnVoltar = findViewById(R.id.btnVoltar);
+        //Retorna usando LAMBDA
+        btnVoltar.setOnClickListener(v -> voltarParaCadastro());
+
+        //Conexão como Banco de dados
+        ProductDatabase db = Room.databaseBuilder(getApplicationContext(), ProductDatabase.class, "product-database")
+                .allowMainThreadQueries()
+                .build();
+
+        ProductDao productDao = db.productDao();
+
+        //Recuperar todos os produtos cadastrados no banco e salvar em uma lista
+
+        List<Product> productList = productDao.getAllProducts();
+        //Construir uma String dentro do laço de repetição
+        StringBuilder report = new StringBuilder();
+
+        //Loop "for-each" para percorrer cada objeto User dentro da lista
+        for (Product product : productList) {
+            report.append("Nome: ").append(product.getName()).append("\n").append("Codigo: ").append(product.getCode()).append("\n").append("Preço: ").append(product.getPrice());
+        }
+        //Exibe o relatório final montado na TextView da tela
+        textViewReport.setText(report.toString());
+
+    }
 }
+
+
